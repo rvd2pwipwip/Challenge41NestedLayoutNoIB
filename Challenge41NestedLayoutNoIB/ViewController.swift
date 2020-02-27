26b6127880446bee1c89fa0e7b5b43aadba545f7
Herve Desrosiers <hdesrosiers@stingray.com>
2020-02-27 16:03:24 -0500

    finished challenge 41

--- Challenge41NestedLayoutNoIB/ViewController.swift
+++ Challenge41NestedLayoutNoIB/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupGreenView()
+        setupRedView()
     }
-
-
+    
+    private func setupGreenView() {
+        view.addSubview(greenView)
+        NSLayoutConstraint.activate([
+            greenView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            greenView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            greenView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            greenView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50)
+        ])
+    }
+    
+    private func setupRedView() {
+        greenView.addSubview(redView)
+        NSLayoutConstraint.activate([
+            redView.centerYAnchor.constraint(equalTo: greenView.centerYAnchor),
+            redView.leadingAnchor.constraint(equalTo: greenView.leadingAnchor, constant: 25),
+            redView.trailingAnchor.constraint(equalTo: greenView.trailingAnchor, constant: -25),
+            redView.heightAnchor.constraint(equalTo: greenView.heightAnchor, multiplier: 0.2)
+        ])
+    }
+    
+    private let greenView: UIView = {
+        let gv = UIView()
+        gv.translatesAutoresizingMaskIntoConstraints = false
+        gv.backgroundColor = .green
+        return gv
+    }()
+    
+    private let redView: UIView = {
+        let rv = UIView()
+        rv.translatesAutoresizingMaskIntoConstraints = false
+        rv.backgroundColor = .red
+        return rv
+    }()
+    
 }
 
